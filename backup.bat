f82a9641dbf90d81a4178fa4624b11ecba90dc11
炜昆 申 <wk-shen@foxmail.com>
2026-05-08 10:48:39 +0800

    自动版本管理: $(date +'m-H:S')

--- backup.bat
+++ backup.bat
@@ -1,10 +1,10 @@
-锘緻echo off
+@echo off
 if "%1"=="h" goto begin
 mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit
 :begin
-:: 杩欓噷寮�濮嬪啓浣犵殑 git 鍚屾鍛戒护
+:: 这里开始写你的 git 同步命令
 
-cd "C:\Users\wk-sh\Desktop\git鐗堟湰绠＄悊"
+cd "C:\Users\wk-sh\Desktop\git版本管理"
 git add .
-git commit -m "鑷姩鐗堟湰绠＄悊: $(date +'%Y-%m-%d %H:%M:%S')"
+git commit -m "自动版本管理: $(date +'%Y-%m-%d %H:%M:%S')"
 git push origin main
