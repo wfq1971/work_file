003883739711e37a74c2e097570f0cde88c4d7c1
炜昆 申 <wk-shen@foxmail.com>
2026-05-08 11:51:14 +0800

    自动版本管理: 2026/05/08 11:51

--- backup.bat
+++ backup.bat
@@ -1,6 +1,6 @@
 @echo off
 set "datestr=%date:~0,10% %time:~0,5%"
-cd "C:\Users\wk-sh\Desktop\git鐗堟湰绠＄悊"
+cd "C:\Users\wk-sh\Desktop\git版本管理"
 git add .
-git commit -m "鑷姩鐗堟湰绠＄悊: %datestr%"
+git commit -m "自动版本管理: %datestr%"
 git push origin main
